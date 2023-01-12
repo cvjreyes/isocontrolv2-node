de34f3693680dbc5b30415d4d700fa6a0d8c6072
Sean Sáez Fuller <sean.saez.f@gmail.com>
2023-01-12 17:25:15 +0100

    edit forecast finished

--- src/main/feed/feed.routes.js
+++ src/main/feed/feed.routes.js
@@ -6,9 +6,13 @@ router.get("/get_progress", checkAuth, controller.getProgress);
 
 router.get("/get_feed_pipes", checkAuth, controller.getFeedPipes);
 
+router.get("/get_forecast", checkAuth, controller.getFeedForecast)
+
 router.post("/submit_feed_pipes", checkAuth, controller.submitFeedPipes);
 
 router.post("/add_pipes", checkAuth, controller.addPipes);
+
+router.post("/submit_forecast", checkAuth, controller.submitForecast);
 
 router.delete("/delete_pipe/:id", checkAuth, controller.deletePipe);
 
