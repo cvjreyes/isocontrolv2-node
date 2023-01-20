92e5291e651653129158ca65b174951d33b0df9b
Sean Sáez Fuller <sean.saez.f@gmail.com>
2023-01-20 09:24:25 +0100

    Cuando se pone modelled 100% en feed se escribe Feed estimated en ifd

--- src/main/feed/feed.microservices.js
+++ src/main/feed/feed.microservices.js
@@ -3,7 +3,7 @@ const pool = require("../../../config/db");
 exports.addPipeToIFD = async (pipe, area_id, line_refno) => {
   const [res] = await pool.query(
     "INSERT INTO ifd_pipes (line_refno, feed_id, area_id, diameter, train, status) VALUES (?, ?, ?, ?, ?, ?)",
-    [line_refno, pipe.id, area_id, pipe.diameter, pipe.train, "ESTIMATED"]
+    [line_refno, pipe.id, area_id, pipe.diameter, pipe.train, "FEED_ESTIMATED"]
   );
   return res;
 };
