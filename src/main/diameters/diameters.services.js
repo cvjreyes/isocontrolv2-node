0a75306e8e25dc549d04d029a1f303ad03cad981
Toni <webdevtoni@gmail.com>
2023-01-30 13:59:06 +0100

    update env name from MMDN to NPSDN

--- src/main/diameters/diameters.services.js
+++ src/main/diameters/diameters.services.js
@@ -1,7 +1,7 @@
 const pool = require("../../../config/db");
 
 exports.getDiametersService = async () => {
-  if (process.env.NODE_MMDN == "1") {
+  if (process.env.NODE_NPSDN == "1") {
     let [diameters] = await pool.query("SELECT dn as diameter FROM diameters");
     return diameters;
   } else {
