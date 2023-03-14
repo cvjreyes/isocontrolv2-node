b6c852bcef131f8910491aec69025e3ef5e5559d
Toni <webdevtoni@gmail.com>
2023-03-14 11:56:49 +0100

    fix

--- src/node_cron/pipes.js
+++ src/node_cron/pipes.js
@@ -131,13 +131,13 @@ exports.saveFEEDWeight = async () => {
         progress += 100;
       }
     }
+    console.log(max_progress, progress);
     await pool.query(
       "INSERT INTO feed_progress(progress, max_progress) VALUES(?,?)",
       [progress, max_progress]
     );
   } catch (err) {
     console.error(err);
-    throw err;
   }
 };
 
